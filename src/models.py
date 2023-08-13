e16b91afd6413efe43ee134dcbb412fc9527979a
Kate Deras <65469487+quackate@users.noreply.github.com>
2023-08-13 20:15:59 +0000

    Diagram Completed

--- src/models.py
+++ src/models.py
@@ -1,29 +1,48 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=True)
+    email = Column(String(250), nullable=False, unique=True)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    type = Column(Enum("characters", "planets"), nullable=False)
+    char_id = Column(Integer, ForeignKey('characters.id'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+
+class Characters(Base):
+    __tablename__= 'characters'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=True)
+    birth_year = Column(String(250), nullable=True)
+    gender = Column(String(250), nullable=False)
+    heigth = Column(Integer, nullable=False)
+    skin_color = Column(String(250), nullable=True)
+    eye_color = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__= 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    diameter = Column(Integer, nullable=False)
+    rotation_period = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    population = Column(Integer, nullable=True)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=True)
 
     def to_dict(self):
         return {}
